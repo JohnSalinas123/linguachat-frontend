72a25930eee840e9ea1c5b8a168c56b424053c9e
JohnSalinas123 <salinasjohn257@gmail.com>
2024-11-14 17:25:17 -0800

    add chat box component, wip

---
+++ frontend/src/components/ChatBox/ChatBox.tsx
@@ -0,0 +1,19 @@
+import { Stack } from "@mantine/core";
+import { MessageBubble } from "../MessageBubble/MessageBubble";
+
+import { Messages, Message } from "../../types/Message";
+
+import "./ChatBox.css"
+
+export const ChatBox = ({ messages }: { messages: Messages }) => {
+	return (
+		<>
+			<Stack className="chat-box">
+				{messages &&
+					messages.messages.map((message: Message, index: number) => (
+						<MessageBubble key={index} message={message} />
+					))}
+			</Stack>
+		</>
+	);
+};
