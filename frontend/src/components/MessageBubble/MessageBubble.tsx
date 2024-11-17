dcbf6ba2d9dfd1ddf23ddd36f5dd05acfd419347
JohnSalinas123 <salinasjohn257@gmail.com>
2024-11-16 17:25:55 -0800

    add message name and timestamp, add proper alignment

--- frontend/src/components/MessageBubble/MessageBubble.tsx
+++ frontend/src/components/MessageBubble/MessageBubble.tsx
@@ -8,15 +8,35 @@ export const MessageBubble = ({ message }: { message: Message }) => {
 	return (
 		<>
 			<Container
-				pt="4px"
-				pb="4px"
-				pl="10px"
-				pr="10px"
-				className={`message-bubble ${
-					message.isUser ? "users-message" : "others-message"
+				className={`message-bubble-outer ${
+					message.isUser ? "users-align" : "others-align"
 				}`}
 			>
-				<Text size="md">{message.text}</Text>
+				<Text
+					className={`message-info ${
+						message.isUser ? "message-info-user" : "message-info-other"
+					}`}
+				>
+					{message.name}
+				</Text>
+				<Container
+					pt="4px"
+					pb="4px"
+					pl="10px"
+					pr="10px"
+					className={`message-bubble ${
+						message.isUser ? "users-message" : "others-message"
+					}`}
+				>
+					<Text size="md">{message.text}</Text>
+				</Container>
+				<Text
+					className={`message-info ${
+						message.isUser ? "message-info-user" : "message-info-other"
+					}`}
+				>
+					{message.timestamp}
+				</Text>
 			</Container>
 		</>
 	);
