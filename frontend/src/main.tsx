bd5b6fb2ff2375e7c84bb2fa100f57c10a998894
JohnSalinas123 <salinasjohn257@gmail.com>
2024-11-16 17:17:41 -0800

    add react-router-dom routing, public rootlayout and secure chatlayouts

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,37 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import RootLayout from "./layouts/RootLayout.tsx";
+import LandingPage from "./pages/LandingPage.tsx";
+import SignInPage from "./pages/SignInPage.tsx";
+import SignUpPage from "./pages/SignUpPage.tsx";
+import ChatLayout from "./layouts/ChatLayout.tsx";
+import { ChatPage } from "./pages/ChatPage.tsx";
+import { MantineProvider } from "@mantine/core";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import "@mantine/core/styles.css";
+
+const router = createBrowserRouter([
+	{
+		element: <RootLayout />,
+		children: [
+			{ path: "/", element: <LandingPage /> },
+			{ path: "/sign-in/*", element: <SignInPage /> },
+			{ path: "/sign-up/*", element: <SignUpPage /> },
+			{
+				element: <ChatLayout />,
+				path: "chat",
+				children: [{ path: "/chat", element: <ChatPage /> }],
+			},
+		],
+	},
+]);
+
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<MantineProvider>
+			<RouterProvider router={router} />
+		</MantineProvider>
+	</StrictMode>
+);
