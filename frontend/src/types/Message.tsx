0b092e012c8c34dcbb5103ebd77a8cce4482a68b
JohnSalinas123 <salinasjohn257@gmail.com>
2024-11-16 17:26:22 -0800

    add additional parameters for Messages and Message types

--- frontend/src/types/Message.tsx
+++ frontend/src/types/Message.tsx
@@ -1,8 +1,15 @@
 export type Messages = {
 	messages: Message[];
+	chatId: string;
+	lastMessage: string;
 };
 
 export type Message = {
+	id: string;
 	text: string;
+	language: string;
 	isUser: boolean;
+	name: string;
+	timestamp: string;
+	status: "sent" | "delivered";
 };
