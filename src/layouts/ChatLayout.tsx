06e069c3c98b744cfdbea932ba0da43a6c25a3d5
JohnSalinas123 <salinasjohn257@gmail.com>
2024-12-21 17:45:49 -0800

    add LangSetWindow to check lang for ChatLayout and children, keeps redirect when sending user to sign-in

--- src/layouts/ChatLayout.tsx
+++ src/layouts/ChatLayout.tsx
@@ -1,9 +1,11 @@
-import { useAuth } from "@clerk/clerk-react";
+import { useAuth, useUser } from "@clerk/clerk-react";
 import { Outlet, useNavigate, useOutletContext } from "react-router-dom";
 import { useEffect } from "react";
+import { LanguageSetWindow } from "../components/LangSetWindow/LangSetWindow";
 
 export default function ChatLayout() {
 	const { userId, isLoaded } = useAuth();
+	const { user } = useUser();
 	const navigate = useNavigate();
 
 	const [disabled, toggleDisabled] =
@@ -11,11 +13,17 @@ export default function ChatLayout() {
 
 	useEffect(() => {
 		if (isLoaded && !userId) {
-			navigate("/sign-in");
+			// fix redirect to invite
+			const currentUrl = window.location.pathname + window.location.search;
+			navigate(`/sign-in?redirect_url=${encodeURIComponent(currentUrl)}`);
 		}
 	}, [isLoaded, navigate, userId]);
 
 	if (!isLoaded) return "Loading...";
+
+	// render modal welcoming user, asking for them to choose a language before
+	// starting to use chat
+	if (!user?.publicMetadata.lang_code) return <LanguageSetWindow />;
 
 	return userId ? (
 		<div className={`page ${disabled ? "page-full" : "page-nav"}`}>
